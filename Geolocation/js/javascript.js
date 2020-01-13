4a2ed91b1498fb6b66fedd75927bcafb6f8854b5
connow3 <43795397+connow3@users.noreply.github.com>
2020-01-12 18:31:40 -0800

    l

--- Geolocation/js/javascript.js
+++ Geolocation/js/javascript.js
@@ -1,12 +1,26 @@
-var map = L.map('map').fitWorld();
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29ubm9yMDAwMCIsImEiOiJjazJwYnM0eHQwMm8xM2VteXJ3ZXd0OXEwIn0.-CAXKsJFls6ZCfjqJNch9A', {
+alert("This page will ask for your location information. This allows you to use the goelocation feature.  Your location information will not be stored or shared.");
+var light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29ubm9yMDAwMCIsImEiOiJjazJwYnM0eHQwMm8xM2VteXJ3ZXd0OXEwIn0.-CAXKsJFls6ZCfjqJNch9A', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v10',
+    id:'mapbox/light-v10',
     tileSize: 512,
-zoomOffset: -1,
-}).addTo(map);
+    zoomOffset: -1,
+});
+
+var dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29ubm9yMDAwMCIsImEiOiJjazJwYnM0eHQwMm8xM2VteXJ3ZXd0OXEwIn0.-CAXKsJFls6ZCfjqJNch9A', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id:'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+});
+var map = L.map('map', {layers:[dark, light]}).fitWorld();
+
+var baseLayers = {
+		"Dark": dark,
+		"Light": light
+	};
+L.control.layers(baseLayers).addTo(map);
 function onLocationFound(e) {
     var radius = e.accuracy; //this defines a variable radius as the accuracy value returned by the locate method divided by 2. It is divided by 2 because the accuracy value is the sum of the estimated accuracy of the latitude plus the estimated accuracy of the longitude. The unit is meters.
 
@@ -19,6 +33,20 @@ function onLocationFound(e) {
       else{
           L.circle(e.latlng, radius, {color: 'red'}).addTo(map);
       }
+      var times = SunCalc.getTimes(new Date(), e.latitude, e.longitude);
+var sunrise = times.sunrise.getHours();
+var sunset = times.sunset.getHours();
+
+
+var currentTime = new Date().getHours();
+    if (sunrise < currentTime && currentTime < sunset){
+      map.removeLayer(dark);
+      map.addLayer(light);
+    }
+    else {
+      map.removeLayer(light);
+      map.addLayer(dark);
+    }
 }
 
 map.on('locationfound', onLocationFound); //this is the event listener
